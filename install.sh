16fa95d850c5debff99589d2550309d6b3a1e3ec
Alexis <45032998+Alexis-BX@users.noreply.github.com>
2022-12-07 20:12:16 -0500

    added cleanups

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ module load cuda/11.0.3 gcc/10.2.0 git
 
 # install cudnn
 cd /ccs/home/$(whoami)/scratch
-cp /gpfs/alpine/csc499/proj-shared/cudnn-linux-ppc64le-8.7.0.84_cuda11-archive.tar.xz /ccs/home/$(whoami)/scratch
+cp /gpfs/alpine/csc499/proj-shared/cudnn-linux-ppc64le-8.7.0.84_cuda11-archive.tar.xz $PWD
 tar -xvf cudnn-linux-ppc64le-8.7.0.84_cuda11-archive.tar.xz
+rm cudnn-linux-ppc64le-8.7.0.84_cuda11-archive.tar.xz
 
 # install NCCL
 cd /ccs/home/$(whoami)/scratch
@@ -20,3 +21,4 @@ make -j 16 src.build NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70" CUDA_HOM
 cd /ccs/home/$(whoami)/scratch
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh
 bash Miniconda3-latest-Linux-ppc64le.sh -b -p $PWD/miniconda3
+rm Miniconda3-latest-Linux-ppc64le.sh
