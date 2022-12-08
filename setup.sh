911aee853b678fe222ef6684b75fdee73ac044d9
Alexis <45032998+Alexis-BX@users.noreply.github.com>
2022-12-07 20:12:36 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+module load cuda/11.0.3 gcc/10.2.0 git
+module list
+
+CONDA_HOME=/ccs/home/$(whoami)/scratch/miniconda3
+NCCL_HOME=/ccs/home/$(whoami)/scratch/nccl/build
+CUDNN_HOME=/ccs/home/$(whoami)/scratch/cudnn-linux-ppc64le-8.7.0.84_cuda11-archive
+
+export LD_LIBRARY_PATH=$CONDA_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$CONDA_HOME/bin:$PATH
+export CPATH=$CONDA_HOME/include:$CPATH
+
+export LD_LIBRARY_PATH=$CUDNN_HOME/lib:$LD_LIBRARY_PATH
