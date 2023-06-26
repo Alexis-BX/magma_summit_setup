3ae978b6c950fff9c6a0559a5989c3f968c46e3b
Alexis <45032998+Alexis-BX@users.noreply.github.com>
2023-06-26 13:43:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-module load cuda/11.0.3 gcc/10.2.0 git
+PROJID=$(cat /ccs/home/$(whoami)/.projectid)
+
+module load cuda/11.1.1 gcc/10.2.0 git openblas cmake
 module list
 
-CONDA_HOME=/ccs/home/$(whoami)/scratch/miniconda3
-NCCL_HOME=/ccs/home/$(whoami)/scratch/nccl/build
-CUDNN_HOME=/ccs/home/$(whoami)/scratch/cudnn-linux-ppc64le-8.7.0.84_cuda11-archive
+CONDA_HOME=/gpfs/alpine/$PROJID/scratch/$(whoami)/miniconda3
+NCCL_HOME=/gpfs/alpine/$PROJID/scratch/$(whoami)/nccl/build
+CUDNN_HOME=/gpfs/alpine/$PROJID/scratch/$(whoami)/cuda/targets/ppc64le-linux
 
 export LD_LIBRARY_PATH=$CONDA_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$CONDA_HOME/bin:$PATH
@@ -14,3 +16,15 @@ export CPATH=$CUDNN_HOME/include:$CPATH
 
 export LD_LIBRARY_PATH=$NCCL_HOME/lib:$LD_LIBRARY_PATH
 export CPATH=$NCCL_HOME/include:$CPATH
+
+# pytorch source-build stuff. Ignore unless you're building torch yourself
+CUDNN_LIB_DIR=$CUDNN_HOME/lib
+CUDNN_INCLUDE_DIR=$CUDNN_HOME/include
+CUDNN_LIBRARY=$CUDNN_HOME
+USE_CUDNN=1
+NCCL_ROOT=$NCCL_HOME
+USE_SYSTEM_NCCL=1
+USE_DISTRIBUTED=1
+USE_NCCL=1
+NCCL_LIBRARIES=$NCCL_HOME/lib
+NCCL_INCLUDE_DIRS=$NCCL_HOME/include
